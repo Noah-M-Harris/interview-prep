6203f0bf8ffc4f0cac6d966ae03e186bda67b5b4
Noah <noahharris.000@gmail.com>
2023-05-12 11:29:37 -0500

    find leftmost idx recrusive solution

--- binary_search/find_leftmost_idx.py
+++ binary_search/find_leftmost_idx.py
@@ -36,9 +36,37 @@ def solution(nums, target):
             lo = mid + 1
     return found
 
+def solution_recursive(nums, target):
+    if target < nums[0]:
+        return 0
+    if target > nums[-1]:
+        return len(nums)
+    
+    found = [-1]
+    def helper(lo, hi, target):
+        if lo > hi:
+            return
+
+        mid = lo + ((hi - lo) // 2)
+
+        if nums[mid] == target:
+            found[0] = mid
+            helper(lo, mid - 1, target)
+        elif nums[mid] > target:
+            helper(lo, mid - 1, target)
+        else:
+            helper(mid + 1, hi, target)  
+
+    helper(0, len(nums)-1, target)
+    return found[0]
+
 nums = [1, 2, 4, 5, 6, 7]
 target = 8
 print(solution(nums, target)) # 6
+
+nums = [1, 2, 4, 5, 6, 7]
+target = 8
+print("recurisve", solution_recursive(nums, target)) # 6
 
 nums = [1, 3, 5, 6, 6, 9] 
 target = 6
@@ -52,6 +80,14 @@ nums = [2, 2, 2, 2, 2, 2, 2, 2]
 target = 2
 print(solution(nums, target)) # 0
 
+nums = [2, 2, 2, 2, 2, 2, 2, 2]
+target = 2
+print("recurisve", solution_recursive(nums, target)) # 0
+
 nums = [1, 3, 3, 3, 5, 7, 8, 12]
 target = 3
 print(solution(nums, target)) # 1
+
+nums = [1, 3, 3, 3, 5, 7, 8, 12]
+target = 3
+print("recurisve", solution_recursive(nums, target)) # 1
--- dynamic_programming/word_break_II.py
+++ dynamic_programming/word_break_II.py
@@ -14,5 +14,17 @@ output = ["cats and dog", "cat sand dog"]
 
 def solution(str, word_dict):
 
-    dict = set(word_dict)
-    result = {}
+    dp = [False] * (len(str) + 1)
+    dp[len(str)] = True
+    word = {}
+
+    for i in range(len(str)-1, -1, -1):
+        for w in word_dict:
+            if (i + len(w)) <= len(str) and str[i: i + len(w)] == w:
+                dp[i] = dp[i + len(w)]
+
+    return dp[0]
+
+s = "catsanddog"
+word_dict = ["cat", "and", "sands", "cats", "dog"]
+print(solution(s, word_dict))
