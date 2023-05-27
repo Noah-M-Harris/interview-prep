ad305cbdc1cf586e1d274674928f70b8d05c030d
Noah <noahharris.000@gmail.com>
2023-05-27 14:16:33 -0500

    house robber fix

--- dynamic_programming/house_robber.py
+++ dynamic_programming/house_robber.py
@@ -7,14 +7,15 @@ def solution(nums):
         a b [#, #, ...]
         when looping over nums, always changing where a and b are, a + n becomes b as we move to the next num, on first run, etc
         """
-        b, a = a + n, b
-    
-    return max(a, b)
+        tmp = max(a + n, b)
+        a = b
+        b = tmp
+    return b
+
 
 
 nums = [1, 3, 5] # 6
 print(solution(nums))
-
 
 nums = [3, 4, 6, 65, 56, 8] # 77
 print(solution(nums))
@@ -24,3 +25,6 @@ print(solution(nums)) # 4
 
 nums = [2,7,9,3,1]
 print(solution(nums)) # 12
+
+nums = [2,1,1,2]
+print(solution(nums)) # 4
