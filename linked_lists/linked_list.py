435fddd6ee9c34061b183184aa35f7778a52eab2
Noah <noahharris.000@gmail.com>
2023-06-06 20:45:15 -0500

    linkedList class, remove_duplicates

---
+++ linked_lists/linked_list.py
@@ -0,0 +1,71 @@
+class Node:
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+
+    def __repr__(self):
+        result = ""
+        result += ", " + str(self.val)
+        curr = self.next
+        while curr:
+            result += ", " + str(curr.val)
+            curr = curr.next
+
+        return "[" + result.strip(", ") + "]"
+
+    def add(self, node):
+        # adds a node to the end of a linkedlist starting at this position
+        if not isinstance(node, Node):
+            node = Node(node)
+
+        if not self.next:
+            self.next = node
+            return True
+        
+        curr = self.next
+        while curr and curr.next:
+            curr = curr.next
+        curr.next = node
+        return True
+
+
+class LL:
+    def __init__(self, root=None):
+        self.root = root
+        self.pos = root
+
+    def add(self, node):
+        
+        if not isinstance(node, Node):
+            node = Node(node)
+
+        self.pos.next = node
+        self.pos = self.pos.next
+    
+    def __repr__(self):
+        curr = self.root
+        result = ""
+        while curr:
+            result += str(curr.val) + " "
+            curr = curr.next
+        return result
+
+root = Node(-1)
+linkedList = LL(root)
+linkedList.add(2)
+node = Node(3)
+linkedList.add(node)
+# print(repr(linkedList))
+# print(str(linkedList))
+print(linkedList)
+
+
+
+root = Node(-1)
+second = Node(0)
+third = Node(1)
+root.next = second
+second.next = third
+third.next = Node(2)
+print(root.next)
+
---
+++ linked_lists/remove_dups.py
@@ -0,0 +1,33 @@
+"""
+given a linked list of integers, remove all duplicate nodes
+"""
+from linked_list import LL, Node
+
+
+def solution(root):
+    # keeps track of all node values already seen
+    buffer = set()
+    dummy = Node(-1)
+    prev = dummy
+
+    # buffer keeps track of all the nodes already present in the new linked list
+    curr = root
+    while curr:
+        if curr.val not in buffer:
+            prev.next = Node(curr.val)
+            buffer.add(curr.val)
+            prev = prev.next
+        curr = curr.next
+    return dummy.next
+
+root = Node(2)
+linkedList = LL(root)
+linkedList.add(34)
+linkedList.add(4)
+linkedList.add(3)
+linkedList.add(23)
+linkedList.add(34)
+linkedList.add(1)
+linkedList.add(37)
+print(solution(linkedList.root))
+
